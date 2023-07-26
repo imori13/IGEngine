f1d3e915183a0a769973f99535178e8fab0f77c2
imori13 <igei0013@gmail.com>
2023-07-26 12:43:46 +0900

    環境構築バッチファイルを作成

--- vcpkg_utility/install_packages.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo packages.txtからすべてのパッケージをインストールします...
-for /F "tokens=1,* delims= " %%i in (packages.txt) do (
-    ..\vcpkg\vcpkg.exe install %%i
-)
-echo すべてのパッケージのインストールが完了しました。
-..\vcpkg\vcpkg.exe integrate install
-pause
